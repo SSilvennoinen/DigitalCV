390d94d875dc71bb651e86de6df7577e5811a25c
Saanaeee <ssaana.silvennoinen@gmail.com>
2025-12-12 13:47:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,152 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+
+    // Modal Video
+    var $videoSrc;
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+    $('#videoModal').on('shown.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    $('#videoModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc);
+    })
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: true,
+        loop: true,
+    });
+
+    
+})(jQuery);
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('https://raw.githubusercontent.com/SSilvennoinen/projektit/refs/heads/main/projektit.json')
+    .then(response => response.json())
+    .then(data => {
+      const container = document.getElementById('projects-container');
+      data.forEach(p => {
+        const item = `
+          <div class="col-lg-4 col-md-6 portfolio-item ${p.category}">
+            <div class="portfolio-img rounded overflow-hidden">
+              <img class="img-fluid" src="${p.image}" alt="${p.title}">
+              <div class="portfolio-btn">
+                <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="${p.image}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="${p.link}" target="_blank"><i class="fa fa-link"></i></a>
+              </div>
+            </div>
+            <h6 class="mt-2">${p.title}</h6>
+          </div>`;
+        container.innerHTML += item;
+      });
+
+      // 🔧 TÄRKEÄ: päivitä Isotope kun kortit on lisätty
+      $('.portfolio-container').isotope('reloadItems').isotope();
+    })
+    .catch(err => console.error(err));
+});
+
